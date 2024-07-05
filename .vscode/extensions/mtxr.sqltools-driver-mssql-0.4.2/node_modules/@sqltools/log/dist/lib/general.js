ec43fe470c455b9049b94be8ea26b8e9a7059354
Christian Bannister <carskb@gmail.com>
2024-07-05 11:14:28 +0100

    Delete .vscode directory

--- .vscode/extensions/mtxr.sqltools-driver-mssql-0.4.2/node_modules/@sqltools/log/dist/lib/general.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-exports.__esModule = true;
-var factory_1 = __importDefault(require("./factory"));
-exports["default"] = factory_1["default"]();
